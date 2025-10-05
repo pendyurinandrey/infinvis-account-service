5b7772b05ee3c805d27367faa8c2c1178c01514e
pendyurinandrey <pendyurinandrey@gmail.com>
2025-10-05 13:18:13 +0300

    Added initial schema

--- src/main/kotlin/com/infinvis/accountservice/App.kt
+++ src/main/kotlin/com/infinvis/accountservice/App.kt
@@ -1,10 +1,9 @@
 package com.infinvis.accountservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 class InfinvisAccountServiceApplication
 
 fun main(args: Array<String>) {
